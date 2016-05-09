8be3b2d71e6ee33784535fd726c162e2a8ca8631
Exellanix <brendanhodgson903@gmail.com>
2016-05-08 21:05:10 -0700

    initial project

---
+++ src/io/github/exellanix/advancedcombat/AdvancedCombat.java
@@ -0,0 +1,10 @@
+package io.github.exellanix.advancedcombat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Brendan on 5/8/2016.
+ */
+public class AdvancedCombat extends JavaPlugin {
+
+}
